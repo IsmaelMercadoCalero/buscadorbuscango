13cf08aa9cef201eaa91b8c536b2ca30ca669d28
Ismael Mercado Calero <ismaelmc206@gmail.com>
2022-04-28 13:05:36 +0200

    kk

--- lib/ClasesPrincipales/BuscangoTarjeta.dart
+++ lib/ClasesPrincipales/BuscangoTarjeta.dart
@@ -14,7 +14,21 @@ class BuscangoTarjeta extends StatefulWidget {
   State<BuscangoTarjeta> createState() => _BuscangoTarjetaState();
 }
 
-class _BuscangoTarjetaState extends State<BuscangoTarjeta> {
+class _BuscangoTarjetaState extends State<BuscangoTarjeta>
+    with SingleTickerProviderStateMixin {
+  late TabController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -23,155 +37,164 @@ class _BuscangoTarjetaState extends State<BuscangoTarjeta> {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       resizeToAvoidBottomInset: false,
-      body: DefaultTabController(
-        length: 4,
-        child: Container(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  color: colorGeneral,
-                  width: double.infinity,
-                  height: alto * 0.15,
-                  child: Row(
-                    children: [
-                      Padding(
-                          padding:
-                              EdgeInsets.symmetric(horizontal: ancho * 0.02)),
-                      Container(
-                        child: Text(
-                          'Buscango',
-                          textAlign: TextAlign.center,
-                          style: textBuscango,
-                        ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                color: colorGeneral,
+                width: double.infinity,
+                height: alto * 0.15,
+                child: Row(
+                  children: [
+                    Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: ancho * 0.02)),
+                    Container(
+                      child: Text(
+                        'Buscango',
+                        textAlign: TextAlign.center,
+                        style: textBuscango,
                       ),
-                      Padding(
-                          padding:
-                              EdgeInsets.symmetric(horizontal: ancho * 0.01)),
-                      Container(
-                          width: ancho * 0.55,
-                          height: alto * 0.06,
-                          child: Buscador()),
-                    ],
-                  ),
+                    ),
+                    Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: ancho * 0.01)),
+                    Container(
+                        width: ancho * 0.55,
+                        height: alto * 0.06,
+                        child: Buscador()),
+                  ],
                 ),
-                Padding(padding: EdgeInsets.symmetric(vertical: alto * 0.08)),
-                Container(
-                  width: size.width * 0.8,
-                  height: size.height * 2,
-                  decoration: BoxDecoration(
-                    color: Colors.blueGrey[100],
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey,
-                        spreadRadius: 5,
-                        offset: Offset(7, 7),
-                        blurRadius: 6,
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20),
-                            topRight: Radius.circular(20)),
-                        child: Column(
-                          children: [
-                            Container(
-                              color: Colors.blueGrey,
-                              width: double.infinity,
-                              height: alto * 0.3,
-                              child: Row(
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: ancho * 0.01,
-                                        vertical: alto * 0.01),
-                                    child: Align(
-                                      alignment: Alignment.topLeft,
-                                      child: Icon(
-                                        Icons.arrow_back,
-                                        color: Colors.white,
-                                        size: 20,
-                                      ),
+              ),
+              Padding(padding: EdgeInsets.symmetric(vertical: alto * 0.08)),
+              Container(
+                width: size.width * 0.8,
+                height: size.height * 2,
+                decoration: BoxDecoration(
+                  color: Colors.blueGrey[100],
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      spreadRadius: 5,
+                      offset: Offset(7, 7),
+                      blurRadius: 6,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20)),
+                      child: Column(
+                        children: [
+                          Container(
+                            color: Colors.blueGrey,
+                            width: double.infinity,
+                            height: alto * 0.3,
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: ancho * 0.01,
+                                      vertical: alto * 0.01),
+                                  child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Icon(
+                                      Icons.arrow_back,
+                                      color: Colors.white,
+                                      size: 20,
                                     ),
                                   ),
-                                  Padding(
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: ancho * 0.2),
-                                    child: Row(children: [
-                                      SizedBox(
-                                        width: ancho * 0.13,
-                                        height: alto * 0.13,
-                                        child: FloatingActionButton(
-                                          child: Padding(
-                                            padding: EdgeInsets.symmetric(
-                                                vertical: alto * 0.04),
-                                            child: Column(
-                                              children: [
-                                                Icon(
-                                                  Icons.image,
-                                                  color: Colors.grey,
-                                                ),
-                                                Text(
-                                                  'Imagen logo',
-                                                  style: TextStyle(
-                                                      color: Colors.blueGrey),
-                                                )
-                                              ],
-                                            ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: ancho * 0.2),
+                                  child: Row(children: [
+                                    SizedBox(
+                                      width: ancho * 0.13,
+                                      height: alto * 0.13,
+                                      child: FloatingActionButton(
+                                        child: Padding(
+                                          padding: EdgeInsets.symmetric(
+                                              vertical: alto * 0.04),
+                                          child: Column(
+                                            children: [
+                                              Icon(
+                                                Icons.image,
+                                                color: Colors.grey,
+                                              ),
+                                              Text(
+                                                'Imagen logo',
+                                                style: TextStyle(
+                                                    color: Colors.blueGrey),
+                                              )
+                                            ],
                                           ),
-                                          backgroundColor: Colors.white,
-                                          onPressed: null,
                                         ),
+                                        backgroundColor: Colors.white,
+                                        onPressed: null,
                                       ),
-                                      Text(
-                                        'NOMBRE TIENDA',
-                                        style: TextStyle(
-                                            fontSize: 30,
-                                            fontWeight: FontWeight.w800,
-                                            color: Colors.white),
-                                      )
-                                    ]),
-                                  ),
-                                ],
-                              ),
+                                    ),
+                                    Text(
+                                      'NOMBRE TIENDA',
+                                      style: TextStyle(
+                                          fontSize: 30,
+                                          fontWeight: FontWeight.w800,
+                                          color: Colors.white),
+                                    )
+                                  ]),
+                                ),
+                              ],
                             ),
-                            Container(
-                              width: ancho * 0.4,
-                              child: TabBar(
-                                indicatorColor: colorGeneral,
-                                labelColor: colorGeneral,
-                                indicatorWeight: 8,
-                                labelStyle: TextStyle(fontSize: 15),
-                                tabs: [
-                                  Tab(
-                                    text: 'Productos/Servicios',
+                          ),
+                          Container(
+                            width: ancho * 0.4,
+                            child: Column(
+                              children: [
+                                DefaultTabController(
+                                  length: 4,
+                                  child: TabBar(
+                                    indicatorColor: colorGeneral,
+                                    labelColor: colorGeneral,
+                                    indicatorWeight: 8,
+                                    labelStyle: TextStyle(fontSize: 15),
+                                    tabs: [
+                                      Tab(
+                                        text: 'Productos/Servicios',
+                                      ),
+                                      Tab(
+                                        text: 'Galería',
+                                      ),
+                                      Tab(
+                                        text: 'Quíenes somos',
+                                      ),
+                                      Tab(
+                                        text: 'Contacto',
+                                      ),
+                                    ],
+                                    controller: controller,
                                   ),
-                                  Tab(
-                                    text: 'Galería',
-                                  ),
-                                  Tab(
-                                    text: 'Quíenes somos',
-                                  ),
-                                  Tab(
-                                    text: 'Contacto',
-                                  ),
-                                ],
-                              ),
+                                ),
+                                TabBarView(
+                                  controller: controller,
+                                  children: [],
+                                )
+                              ],
                             ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-                Padding(padding: EdgeInsets.symmetric(vertical: alto * 0.1)),
-                PiePagina()
-              ],
-            ),
+              ),
+              Padding(padding: EdgeInsets.symmetric(vertical: alto * 0.1)),
+              PiePagina()
+            ],
           ),
         ),
       ),
