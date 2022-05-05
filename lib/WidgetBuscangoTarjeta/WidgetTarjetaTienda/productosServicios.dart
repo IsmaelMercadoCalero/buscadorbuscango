f06eec49a73afcd3b29fc7fdc3a8b7e53b41c69a
Ismael Mercado Calero <ismaelmc206@gmail.com>
2022-05-05 09:30:49 +0200

    kk

---
+++ lib/WidgetBuscangoTarjeta/WidgetTarjetaTienda/productosServicios.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class productosServicios extends StatefulWidget {
+  @override
+  State<productosServicios> createState() => _productosServiciosState();
+}
+
+class _productosServiciosState extends State<productosServicios> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
